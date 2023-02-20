7f39e3069efb883522c2ece498d312d19f82f457
Samuel Giorgievski <SamGiorgievski@gmail.com>
2023-02-20 15:33:14 -0500

    search functionality

--- src/stores/HomeStore.js
+++ src/stores/HomeStore.js
@@ -14,7 +14,19 @@ const homeStore = create((set) => ({
   searchCoins: debounce(async () => {
     const {query} = homeStore.getState()
     const res = await axios.get(`https://api.coingecko.com/api/v3/search?query=${query}`)
-    console.log(res);
+    
+    const coins = res.data.coins.map(coin => {
+      return {
+        name: coin.name,
+        image: coin.large,
+        id: coin.id,
+        rank: coin.market_cap_rank,
+        symbol: coin.symbol,
+        logo: coin.thumb
+      }
+    })
+
+    set({coins})
   }, 500),
 
  fetchCoins: async () => {
@@ -26,7 +38,6 @@ const homeStore = create((set) => ({
       image: coin.item.large,
       id: coin.item.id,
       priceBTC: coin.item.price_btc,
-      picture: coin.item.large,
       logo: coin.item.thumb,
       symbol: coin.item.symbol,
       rank: coin.item.market_cap_rank
